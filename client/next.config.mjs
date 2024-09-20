181a9d2fbf201f5f6803a9f816e5b5bf2576db20
Yadhukrishnan <yadhukrishnan2030@gmail.com>
2024-09-20 10:01:04 +0530

    First commit

---
+++ client/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+      NEXT_PUBLIC_FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL,
+    },
+  };
+  
+  export default nextConfig;
+  
